aeb20c014bbec5eba3228ad99184de93fd67e378
ippei-tanaka <tanaka.ippei.jp@gmail.com>
2021-07-20 18:04:54 -0400

    Scratch

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  const comeback = await prisma.artist.create({
+    data: {
+      name: 'Comeback Kid',
+      url: 'https://comeback-kid.com/',
+      albums: {
+        create: [
+          {name: 'Turn It Around', year: '2003'},
+          {name: 'Wake the Dead', year: '2005'},
+        ],
+      },
+    },
+  });
+  console.log({comeback});
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
