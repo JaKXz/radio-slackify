e85b42df356d5c89009ba93831f99698747810e4
Sam Bull <sam.bull@shopify.com>
2021-07-20 19:58:58 -0400

    Reconfigure our api route.
    Now it works with the apollo graphql query tool

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -3,9 +3,30 @@ import { schema } from "../../graphql-schema";
 import { context } from "../../graphql-context";
 import {NextApiRequest, NextApiResponse} from 'next';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const server = new ApolloServer({ schema, context });
-  await server.start();
-  const handler = server.createHandler({ path: "/api/graphql" });
-  return handler(req, res);
-};
+const server = new ApolloServer({ schema, context });
+const startServer = server.start()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  // Headers to make us compatible with the nice graphql UI
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Headers', 'content-type')
+  res.setHeader('Access-Control-Allow-Origin', 'https://studio.apollographql.com')
+  res.setHeader('Vary', 'Origin');
+
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+
+  await startServer
+  await server.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
