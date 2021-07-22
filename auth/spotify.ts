1b8f7c30dc7fd7f466177ed7013916154b5e31da
Jason Kurian <jason.kurian@shopify.com>
2021-07-22 11:33:47 -0400

    fix spotify scopes and stringification

--- auth/spotify.ts
+++ auth/spotify.ts
@@ -2,21 +2,20 @@ import {stringify} from 'query-string';
 
 const authEndpoint: string = 'https://accounts.spotify.com/authorize';
 
-const scope: string[] = [
-  'playlist-read-collaborative',
-  'playlist-modify-public',
-  'user-top-read',
-  'playlist-read-private',
-  'playlist-modify-private',
-  'user-follow-read',
-  'user-read-recently-played',
+const scopes: string[] = [
+  'user-library-modify',
   'user-library-read',
+  'streaming',
+  'user-read-email',
+  'user-read-private',
+  'user-modify-playback-state',
+  'user-read-playback-state',
 ];
 
 export const spotifyLoginUrl = `${authEndpoint}?${stringify({
   client_id: process.env.SPOTIFY_CLIENT_ID,
   response_type: 'token',
   redirect_uri: `http://localhost:3000/api/spotify-callback`,
-  scope,
+  scope: scopes.join('%20'),
   state: '123',
 })}`;
