8daa07a9b23b30b3142e7ecb4ee14dfcefa04103
ippei-tanaka <tanaka.ippei.jp@gmail.com>
2021-07-24 20:36:48 -0400

    Fix the bug in use-local-storage due to server side rendering

--- hooks/use-local-storage.ts
+++ hooks/use-local-storage.ts
@@ -4,6 +4,7 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === 'undefined') return initialValue;
     try {
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
@@ -15,9 +16,11 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
       return initialValue;
     }
   });
+
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
   const setValue = (value: T | ((val: T) => T)) => {
+    if (typeof window === 'undefined') return;
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
@@ -31,5 +34,6 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
       console.log(error);
     }
   };
+
   return [storedValue, setValue] as const;
 }
