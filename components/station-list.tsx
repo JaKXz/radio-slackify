e8fb0246c979a284e638f58209280faf897b6dab
ippei-tanaka <tanaka.ippei.jp@gmail.com>
2021-08-01 20:56:21 -0400

    Remove warning about station-list.tsx

--- components/station-list.tsx
+++ components/station-list.tsx
@@ -1,10 +1,14 @@
-import {useState, useEffect, useMemo} from 'react';
+import {useState, useEffect} from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 import {useQuery, gql} from '@apollo/client';
 import {NexusGenFieldTypes} from '../graphql/nexus';
 import Link from 'next/link';
 
 type Query = NexusGenFieldTypes['Query'];
+type Station = {
+  id: number;
+  name: string;
+};
 
 const GET_STATION_LIST = gql`
   query Query {
@@ -20,12 +24,23 @@ const GET_STATION_LIST = gql`
 
 export default function StationList() {
   const {loading, error, data} = useQuery<Query>(GET_STATION_LIST);
+  const [stations, setStations] = useState([] as Station[]);
+
+  useEffect(() => {
+    if (data) {
+      setStations(data.stations);
+    }
+    return () => {
+      setStations([]);
+    };
+  }, [data]);
+
   return (
     <div>
       <h3>Stations</h3>
-      {data && (
+      {stations.length > 0 && (
         <ul>
-          {data.stations.map((station) => (
+          {stations.map((station) => (
             <li key={station.id}>
               <Link href={`/stations/${station.id}`}>{station.name}</Link>
             </li>
